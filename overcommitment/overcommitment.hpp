1289fc1e964bb2e613639e9104059851b5f5f5ba
JBludau <jakob.bludau@tum.de>
2022-07-01 17:17:40 +0000

    added overcommitment with preload

--- overcommitment/overcommitment.hpp
+++ overcommitment/overcommitment.hpp
@@ -6,11 +6,33 @@
 
 TEST(hip, Overcommitment) {
   auto devProp = getDeviceProperties(0);
-  int* ptr;
-  HIP_SAFE_CALL(hipHostMalloc(&ptr, 1.2 * devProp.totalGlobalMem));
+  int* ptr     = nullptr;  // as we might call free on it without allocating
+                           // anything
+  ASSERT_NO_THROW(
+      HIP_SAFE_CALL(hipHostMalloc(&ptr, 1.2 * devProp.totalGlobalMem)));
   HIP_SAFE_CALL(hipFree(ptr));
-  HIP_SAFE_CALL(hipMallocManaged(&ptr, 1.2 * devProp.totalGlobalMem));
+  ASSERT_NO_THROW(
+      HIP_SAFE_CALL(hipMallocManaged(&ptr, 1.2 * devProp.totalGlobalMem)));
   HIP_SAFE_CALL(hipFree(ptr));
+}
+
+TEST(hip, overcommitment_after_inital_alloc) {
+  auto devProp      = getDeviceProperties(0);
+  int* pre_load_ptr = nullptr;
+  ASSERT_NO_THROW(
+      HIP_SAFE_CALL(hipMalloc(&pre_load_ptr, 0.8 * devProp.totalGlobalMem)));
+
+  int* host_pinned_ptr = nullptr;
+  ASSERT_NO_THROW(HIP_SAFE_CALL(
+      hipHostMalloc(&host_pinned_ptr, 0.5 * devProp.totalGlobalMem)));
+  HIP_SAFE_CALL(hipFree(host_pinned_ptr));
+
+  int* managed_ptr = nullptr;
+  ASSERT_NO_THROW(
+      HIP_SAFE_CALL(hipHostMalloc(&managed_ptr, 0.5 * devProp.totalGlobalMem)));
+  HIP_SAFE_CALL(hipFree(managed_ptr));
+
+  HIP_SAFE_CALL(hipFree(pre_load_ptr));
 }
 
 #endif
